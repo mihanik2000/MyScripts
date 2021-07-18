3f997da2b88b8a9618f52ad5f332b4b85061cf26
Mikhail <mihanik2000@mail.ru>
2021-07-18 20:14:08 +0300

    Наведение порядка
    Добавил некоторые проверки
    Добавил поясняющую печать в консоль

--- SetNotepad++AsDefaultEditor.bat
+++ SetNotepad++AsDefaultEditor.bat
@@ -1,12 +1,39 @@
+@echo off
 REM ****************************************
 REM
 REM Written by Michael Medvedev aka mihanik.
 REM
 REM https://mihanik.net
 REM
+REM Require administrator rights: YES
+REM
 REM ****************************************
 
-    If exist "%ProgramFiles%\Notepad++\notepad++.exe" ( reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f )
-    If exist "%ProgramFiles(x86)%\Notepad++\notepad++.exe" ( reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles(x86)%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f )
+rem ****************************************************************************************
+rem Проверяем наличие у пользователя админских прав...
+rem ****************************************************************************************
+
+
+SET HasAdminRights=0
+
+FOR /F %%i IN ('WHOAMI /PRIV /NH') DO (
+	IF "%%i"=="SeTakeOwnershipPrivilege" SET HasAdminRights=1
+)
+
+IF NOT %HasAdminRights%==1 (
+	ECHO .
+	ECHO You need administrator rights to run!
+	ECHO .
+	GOTO END
+)
+
+If exist "%ProgramFiles%\Notepad++\notepad++.exe" ( reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f )
+If exist "%ProgramFiles(x86)%\Notepad++\notepad++.exe" ( reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles(x86)%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f )
+
+echo "A reboot is required to apply changes"
+
+:END
+
+pause 
 
 EXIT /B
