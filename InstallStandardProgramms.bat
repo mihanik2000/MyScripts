201cf0ce7825416235d1322880ef3edf98544fe0
mihanik2000 <mihanik2000@mail.ru>
2024-02-27 11:55:06 +0300

    Update InstallStandardProgramms.bat

--- InstallStandardProgramms.bat
+++ InstallStandardProgramms.bat
@@ -35,82 +35,82 @@ IF NOT %HasAdminRights%==1 (
 )
 
 REM Включение режима электропитания "Высокая производительность"
-CALL %ScriptPath%\Powercfg\EnableHighPerformance.bat
+CALL "%ScriptPath%\Powercfg\EnableHighPerformance.bat"
 
 REM Отключаем спящий режим
-CALL %ScriptPath%\Powercfg\DisableHibernation
+CALL "%ScriptPath%\Powercfg\DisableHibernation
 
 REM Настраиваем учётные записи пользователей на компьютере
-CALL %ScriptPath%\UserManagement\SettingUpUserAccounts.bat
+CALL "%ScriptPath%\UserManagement\SettingUpUserAccounts.bat"
 
 REM Запланируем ежедневное выключение в полночь
-CALL %ScriptPath%\WindowsScheduler\EnableDailyShutdown.bat
+CALL "%ScriptPath%\WindowsScheduler\EnableDailyShutdown.bat"
 
 REM Выполняем минимальную настройку межсетевого экрана
-CALL %ScriptPath%\WindowsFirewall\InitialFirewallSetup.bat
+CALL "%ScriptPath%\WindowsFirewall\InitialFirewallSetup.bat"
 
 REM Заблокируем телеметрию Windows
-CALL %ScriptPath%\WindowsFirewall\BlockMicrosoftTelemetry.bat
+CALL "%ScriptPath%\WindowsFirewall\BlockMicrosoftTelemetry.bat"
 
 REM Добавляем утилиту certutil.exe в исключения межсетевого экрана Windows
-CALL %ScriptPath%\WindowsFirewall\AddCertutilToWindowsFirewallExceptions.bat
+CALL "%ScriptPath%\WindowsFirewall\AddCertutilToWindowsFirewallExceptions.bat"
 
 REM Настроим возможность подключаться по RDP
-CALL %ScriptPath%\WindowsServices\EnableRDPService.bat
+CALL "%ScriptPath%\WindowsServices\EnableRDPService.bat"
 
 REM Установим Curl
-CALL %ScriptPath%\ProgramManagement\Curl-Install.bat
+CALL "%ScriptPath%\ProgramManagement\Curl-Install.bat"
 
 REM Установим Wget
-CALL %ScriptPath%\ProgramManagement\Wget-Install.bat
+CALL "%ScriptPath%\ProgramManagement\Wget-Install.bat"
 
 REM Установим RDP Wrapper
-CALL %ScriptPath%\ProgramManagement\RDPWrapper.bat
+CALL "%ScriptPath%\ProgramManagement\RDPWrapper.bat"
 
 REM Установим dotNetFx3.5
-CALL %ScriptPath%\ProgramManagement\dotNetFx3.5-install.bat
+CALL "%ScriptPath%\ProgramManagement\dotNetFx3.5-install.bat"
 
 REM Установим dotNetFx4.8
-CALL %ScriptPath%\ProgramManagement\dotNetFx4.8-install.bat
+CALL "%ScriptPath%\ProgramManagement\dotNetFx4.8-install.bat"
 
 REM Установим 7Zip
-CALL %ScriptPath%\ProgramManagement\7Zip-Install.bat
+CALL "%ScriptPath%\ProgramManagement\7Zip-Install.bat"
 
 REM Установим Duplicati 2
-CALL %ScriptPath%\ProgramManagement\Duplicati-Install.bat
+CALL "%ScriptPath%\ProgramManagement\Duplicati-Install.bat"
 
 REM Установим LibreOffice
-CALL %ScriptPath%\ProgramManagement\LibreOffice-Install.bat
+CALL "%ScriptPath%\ProgramManagement\LibreOffice-Install.bat"
 
 REM Установим Java SE Runtime Environment
-CALL %ScriptPath%\ProgramManagement\JavaRE-Install.bat
+CALL "%ScriptPath%\ProgramManagement\JavaRE-Install.bat"
 
 REM Установим Unreal Commander
-CALL %ScriptPath%\ProgramManagement\UnrealCommander-Install.bat
+CALL "%ScriptPath%\ProgramManagement\UnrealCommander-Install.bat"
 
 REM Установим Notepad++
-CALL %ScriptPath%\ProgramManagement\Notepad++-Install.bat
+CALL "%ScriptPath%\ProgramManagement\Notepad++-Install.bat"
 
 REM Установим Google Chrome
-CALL %ScriptPath%\ProgramManagement\GoogleChrome-Install.bat
+CALL "%ScriptPath%\ProgramManagement\GoogleChrome-Install.bat"
 
 REM Установим Firefox
-CALL %ScriptPath%\ProgramManagement\Firefox-Install.bat
+CALL "%ScriptPath%\ProgramManagement\Firefox-Install.bat"
 
 REM Установим Thunderbird
-CALL %ScriptPath%\ProgramManagement\Thunderbird-Install.bat
+CALL "%ScriptPath%\ProgramManagement\Thunderbird-Install.bat"
 
 REM Установим LiteManager Pro
-CALL %ScriptPath%\ProgramManagement\LiteManager-Install.bat
+CALL "%ScriptPath%\ProgramManagement\LiteManager-Install.bat"
 
 REM Установим Adobe Acrobat Reader
-CALL %ScriptPath%\ProgramManagement\AcrobatReader-Install.bat
+CALL "%ScriptPath%\ProgramManagement\AcrobatReader-Install.bat"
 
 REM Установим K-Lite_Codec_Pack
-CALL %ScriptPath%\ProgramManagement\KLiteCodecPack-Install.bat
+CALL "%ScriptPath%\ProgramManagement\KLiteCodecPack-Install.bat"
 
 REM Установим AIMP
-CALL %ScriptPath%\ProgramManagement\AIMP-Install.bat
+CALL "%ScriptPath%\ProgramManagement\AIMP-Install.bat"
 
     ECHO .
     ECHO Всё!
